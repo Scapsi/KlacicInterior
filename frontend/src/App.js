d0beb1fa9aedb0f356a60708fc8b108cd5bf0550
scapsi <frankuladpp@gmail.com>
2023-05-28 00:43:15 +0100

    Simple-ecommerce

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">klacic-interiors</a>
+      </header>
+      <main>
+        home page
+      </main>
+    </div>
+  );
+}
+
+export default App;
